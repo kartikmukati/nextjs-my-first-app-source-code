f0507415097d2601e446c263111cbb72733583f3
kartikmukati <kartikmukati2021@gmail.com>
2023-09-04 01:54:27 +0530

    routing-navigation project source code

---
+++ src/app/events/[...slug]/page.js
@@ -0,0 +1,9 @@
+const FilteredEvent = () => {
+    return (
+        <div>
+            <h1>Filtered Event Page</h1>
+        </div>
+    )
+}
+
+export default FilteredEvent;
---
+++ src/app/events/[eventId]/page.js
@@ -0,0 +1,9 @@
+const EventDetail = () => {
+    return (
+        <div>
+            <h1>Event Detail Page</h1>
+        </div>
+    )
+}
+
+export default EventDetail;
